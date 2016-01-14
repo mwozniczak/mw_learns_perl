9851154f7409405eadc9f4e7f46d05f5e0faf37a
Michał Woźniczak <michal.wozniczak@gmail.com>
2016-01-14 14:08:27 +0100

    Improved 4-3

--- cw4/3.pl
+++ cw4/3.pl
@@ -1,7 +1,7 @@
 print "Podaj katalog: ";
 chomp ($dir = <STDIN>);
 
-$dir = $ENV{'HOME'} if !$dir or $dir =~ /^\s+$/;
+$dir = $ENV{'HOME'} if $dir =~ /^\s*$/;
 -d $dir or die "Nie udało się :C\n";
 
 print "$_\n" for (<$dir/*>);
